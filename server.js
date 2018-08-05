2bcd28ccc0d328c3f0e5f7d13d77f49499618d75
blaud <iblaudi@gmail.com>
2018-08-05 14:39:24 +0700

    Deploy heroku attempt 2 (static directory)

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ let mongoose = require('mongoose');
 
 let passport = require('passport');
 let app = express();
+app.use(express.static(__dirname + '/app'));
 app.set('views', path.join(__dirname, 'assets/template'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
